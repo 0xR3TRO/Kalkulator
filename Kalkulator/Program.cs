cc6fe34eff4caf3e6b15dfef3533285f7c5e7c70
NetRunner <126574394+0xR3TRO@users.noreply.github.com>
2024-10-01 09:29:29 +0200

    0.0.1.0
    Wersja stabilna z poprawnym wykorzystaniem funkcji, bez zastosowania pętli.

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        // Deklaracja zmiennych do przechowywania dwóch liczb
+        double liczba1, liczba2;
+        string operacja;
+
+        // Pobranie od użytkownika pierwszej liczby
+        Console.Write("Wprowadź pierwszą liczbę: ");
+        liczba1 = Convert.ToDouble(Console.ReadLine());
+
+        // Pobranie od użytkownika drugiej liczby
+        Console.Write("Wprowadź drugą liczbę: ");
+        liczba2 = Convert.ToDouble(Console.ReadLine());
+
+        // Pobranie od użytkownika wybranej operacji
+        Console.Write("Wprowadź operację (+, -, *, /): ");
+        operacja = Console.ReadLine();
+
+        // Zmienna do przechowywania wyniku
+        double wynik = 0;
+
+        // Instrukcja switch do wyboru odpowiedniej operacji
+        switch (operacja)
+        {
+            case "+":
+                wynik = liczba1 + liczba2; // Dodawanie
+                break;
+
+            case "-":
+                wynik = liczba1 - liczba2; // Odejmowanie
+                break;
+
+            case "*":
+                wynik = liczba1 * liczba2; // Mnożenie
+                break;
+
+            case "/":
+                // Sprawdzenie, czy dzielenie przez zero nie ma miejsca
+                if (liczba2 != 0)
+                {
+                    wynik = liczba1 / liczba2; // Dzielenie
+                }
+                else
+                {
+                    Console.WriteLine("Błąd: Nie można dzielić przez zero!");
+                    return; // Zakończenie programu w przypadku błędu
+                }
+                break;
+
+            default:
+                // Obsługa błędnej operacji
+                Console.WriteLine("Błąd: Nieznana operacja!");
+                return;
+        }
+
+        // Wyświetlenie wyniku
+        Console.WriteLine("Wynik: " + wynik);
+    }
+}
